c85d78861773842fbf126c7db59c6fa7d5db66c0
Ksenia Litovkina <kseniaa22@gmail.com>
2023-03-12 23:15:46 +0100

    add js to subscribe section

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     nav.classList.remove("visible-nav");
 })
+
+//subscribe section: email validation and add thank you
+const subscribeButton = document.getElementById("subscribe");
+const emailInput = document.querySelector(".delete-subscribe input[type='email']");
+
+function isValidEmail(email) {
+    // regex pattern for email validation
+    const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailValidation.test(email);
+  }
+
+subscribeButton.addEventListener("click", function() {
+  // check if the email is valid
+  const email = emailInput.value;
+  if (!isValidEmail(email)) {
+    alert("Please enter a valid email.");
+    return;
+  }
+  
+  // remove the subscribe form
+  const subscribeSection = document.querySelector(".delete-subscribe");
+  subscribeSection.remove();
+  
+  // add the thank you message
+  const questionsSection = document.querySelector(".questions");
+  const thankYouText = document.createElement("div");
+  thankYouText.classList.add("thank-you-message")
+  thankYouText.innerText = "Thank you for subscribing!";
+  questionsSection.appendChild(thankYouText);
+});
+
